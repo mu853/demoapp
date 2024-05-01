496903e099baa86da769b4f43a27ae8f16d2d435
mu853 <mu.youichi853@gmail.com>
2024-05-01 10:02:57 +0000

    update

--- src/main/java/com/my/demo/DemoApplication.java
+++ src/main/java/com/my/demo/DemoApplication.java
@@ -14,6 +14,6 @@ public class DemoApplication {
 	}
 	@GetMapping
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!!!!!!", name);
+		return String.format("Hello [%s]", name);
 	}
 }
