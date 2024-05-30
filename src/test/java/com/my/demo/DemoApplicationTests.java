3f6621b74ce3d9579761add8fd465e01d2604581
mu853 <mu.youichi853@gmail.com>
2024-05-30 21:51:00 +0000

    aaa

--- src/test/java/com/my/demo/DemoApplicationTests.java
+++ src/test/java/com/my/demo/DemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.my.demo;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testHello(@Autowired MockMvc mvc) throws Exception {
+		mvc.perform(get("/"))
+			.andExpect(status().isOk())
+			.andExpect(content().string("Hello [World] !"));
 	}
-
 }
