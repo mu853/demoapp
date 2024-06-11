810df5680e04982d6f9d0ade3009ccf63320fe9f
mu853 <mu.youichi853@gmail.com>
2024-06-11 23:41:55 +0000

    aaa

--- src/main/java/com/my/demo/MessageController.java
+++ src/main/java/com/my/demo/MessageController.java
@@ -3,26 +3,24 @@ package com.my.demo;
 import java.util.List;
 
 import org.springframework.ui.Model;
-import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MessageController {
     @Autowired
     MessageRepository messageRepository;
 
     @GetMapping("messages")
-    public String getMessage(Model model) {
-        List<Message> messages = messageRepository.findAll();
-        model.addAttribute("messages", messages);
-        return "messages";
+    public List<Message> getMessage(Model model) {
+        return messageRepository.findAll();
     }
 
     @PostMapping("messages")
-    public String postMessages(Message message) {
+    public Message postMessages(Message message) {
         messageRepository.save(message);
-        return "redirect:messages";
+        return message;
     }
 }
